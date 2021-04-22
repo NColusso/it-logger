e412d8e95a2e2f509f921e0e2ae00760139d5926
Natasha <natashacolusso@gmail.com>
2021-04-21 19:05:38 -0700

    Feature: Delete techs from db

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -6,6 +6,8 @@ import {
   TECHS_ERROR,
 } from "./types";
 
+const headers = { "Content-Type": "application/json" };
+
 // Get techs from db
 export const getTechs = () => async (dispatch) => {
   try {
@@ -17,6 +19,51 @@ export const getTechs = () => async (dispatch) => {
     dispatch({
       type: GET_TECHS,
       payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
+// Add technician to db
+export const addTech = (tech) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/techs", {
+      method: "POST",
+      body: JSON.stringify(tech),
+      headers,
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
+// Delete from db
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: "DELETE",
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id,
     });
   } catch (err) {
     dispatch({
